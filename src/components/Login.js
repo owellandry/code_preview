d13dce8660ab452053865134ab3c1d4a6a342b6c
owell andry <81540116+owellandry@users.noreply.github.com>
2023-07-24 03:47:16 +0000

    Update files

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,7 @@ import './css/Login.css';
 const Login = () => {
   const handleLogin = () => {
     // Redirige al usuario a la página de autorización de GitHub
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=04fb02454ab303b6b803&scope=user`;
+    window.location.href = `https://owellandry.github.io/code_preview/auth/callback`;
   };
 
   return (
